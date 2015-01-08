610fdff2d914778f3503edee8fb17f2ef06ca6ef
Adam Gleave <adgleave@gmail.com>
2015-01-08 17:58:44 +0000

    Add unit tests for task assignment

---
+++ src/tests/graphs/task_assignments.s
@@ -0,0 +1,65 @@
+c CS 4.6
+c Commercial use requires a licence
+c contact igsys@eclipse.net
+c
+c nodes:              23     arcs:               50
+c scale-factor:       12     cut-off-factor:   12.0
+c
+c time:             0.00     cost:              330
+c refines:             3     discharges:         50
+c pushes:             57     relabels:           32
+c updates:             2     u-scans:             0
+c p-refines:           5     r-scans:            78
+c dfs-scans:         253     bad-in:        0  +  0
+c
+s     330
+f       2      18          0
+f       2      11          0
+f       2       9          0
+f       2      16          1
+f       2       8          0
+f       3       8          0
+f       3      11          0
+f       3      10          0
+f       3      18          1
+f       3      17          0
+f       4       8          0
+f       4      11          1
+f       4      10          0
+f       5       8          0
+f       5      11          0
+f       5      20          0
+f       5      23          1
+f       6       8          0
+f       6      11          0
+f       6      16          0
+f       6      22          1
+f       7       8          0
+f       7      11          0
+f       7      10          1
+f       8       1          0
+f       9      22          0
+f       9      16          0
+f      10      23          0
+f      10      17          1
+f      10      20          0
+f      11      14          1
+f      11      13          0
+f      11      12          0
+f      11      15          0
+f      12      16          0
+f      12      17          0
+f      13      18          0
+f      13      19          0
+f      14      21          0
+f      14      20          1
+f      15      22          0
+f      15      23          0
+f      16       1          1
+f      17       1          1
+f      18       1          1
+f      19       1          0
+f      20       1          1
+f      21       1          0
+f      22       1          1
+f      23       1          1
--- src/tests/test_task_assignment.cpp
+++ src/tests/test_task_assignment.cpp
@@ -19,11 +19,17 @@ protected:
 	fs::FlowNetwork *original;
 	fs::FlowNetwork *augmented;
 
-	TaskAssignmentTest() {
-		graph_file.open("graph_4m_2crs_6j.in");
-		solved_file.open("graph_4m_2crs_6j.s");
+	const std::string GRAPH_PATH = CMAKE_SRC_DIR "/tests/graphs/";
+	const std::string GRAPH_FILENAME = GRAPH_PATH + "task_assignments.in";
+	const std::string SOLUTION_FILENAME = GRAPH_PATH + "task_assignments.s";
+	void SetUp() {
+		graph_file.open(GRAPH_FILENAME, std::fstream::in);
+		ASSERT_FALSE(graph_file.fail()) << "unable to open " << GRAPH_FILENAME;
+		solved_file.open(SOLUTION_FILENAME, std::fstream::in);
+		ASSERT_FALSE(solved_file.fail()) << "unable to open " << SOLUTION_FILENAME;
 
 		original = fs::DIMACS<fs::FlowNetwork>::readDIMACSMin(graph_file);
+		ASSERT_NE(original, (void *)0) << "DIMACS parse error";
 		augmented = new fs::FlowNetwork(*original);
 		fs::DIMACS<fs::FlowNetwork>::readDIMACSMinFlow(solved_file, *augmented);
 	}
@@ -34,11 +40,14 @@ TEST_F(TaskAssignmentTest, Constructor) {
 
 	const std::unordered_set<uint32_t> &tasks = ta.getTasks();
 	std::unordered_set<uint32_t> expected_tasks = {2,3,4,5,6,7};
-	EXPECT_EQ(tasks, expected_tasks);
+	EXPECT_EQ(expected_tasks, tasks);
 
 	const std::unordered_set<uint32_t> &leaves = ta.getLeaves();
-	std::unordered_set<uint32_t> expected_leaves = {16,17,18,19,20,21,22,23};
-	EXPECT_EQ(leaves, expected_leaves);
+	std::unordered_set<uint32_t> expected_leaves = {
+			8, // unscheduled aggregator
+			16,17,18,19,20,21,22,23 // cores
+	};
+	EXPECT_EQ(expected_leaves, leaves);
 }
 
 TEST_F(TaskAssignmentTest, Assignments) {
@@ -56,6 +65,6 @@ TEST_F(TaskAssignmentTest, Assignments) {
 			{ 7, 17 }
 	};
 
-	EXPECT_EQ(*assignments, expected_assignments);
+	EXPECT_EQ(expected_assignments, *assignments);
 	delete assignments;
 }
