9b80e773f84606083fb3260cd95db96de334f1ca
Adam Gleave <adgleave@gmail.com>
2015-01-12 20:58:06 +0000

    benchmark suite now working: test runner implemented

--- src/tests/benchmark.py
+++ src/tests/benchmark.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-import config, sh, shutil, os
+import config, sh, shutil, csv, os, sys, time
 
-# TODO: let user specify the tests to run on CLI
-tests = config.TESTS
+def versionDirectory(version):
+  return os.path.join(config.WORKING_DIRECTORY, version)
 
 def findImplementations(tests):
   implementation_names = set()
@@ -31,7 +31,7 @@ def clean():
   os.mkdir(config.WORKING_DIRECTORY)
 
 def gitCheckout(version):
-  directory = os.path.join(config.WORKING_DIRECTORY, version)
+  directory = versionDirectory(version)
   os.mkdir(directory)
   # how to checkout branch to new working directory, see:
   # htp://blog.jessitron.com/2013/10/git-checkout-multiple-branches-at-same.html
@@ -46,9 +46,10 @@ def build(version, targets):
   
   os.mkdir(build_directory)
   os.chdir(build_directory)
-  # TODO: logging?
   sh.cmake(source_directory)
-  sh.make(*targets)
+  sh.make(*targets, 
+          _out=os.path.join(build_directory, "makefile.out"),
+          _err=os.path.join(build_directory, "makefile.err"))
   
   os.chdir(saved_cwd)
   
@@ -56,11 +57,108 @@ def buildImplementations(implementations):
   build_targets = findBuildTargets(implementations)
   
   clean()
-  for version in build_targets:
+  for version, targets in build_targets.items():
+    print(version, end="")
     gitCheckout(version)
-    build(version, build_targets[version])
+    print(": checked out, ", end="")
+    build(version, targets)
+    print(" built ", targets)
+    
+class ExitCodeException(Exception):
+  def __init__(self, exit_code):
+    self.exit_code = exit_code
+    
+  def __str__(self):
+    return repr(self.exit_code)
+
+def runTestInstance(instance, fname, iteration):
+  # create instance
+  implementation_name = instance["implementation"]
+  implementation = implementations[implementation_name]
+  
+  version_directory = versionDirectory(implementation["version"])
+  exe_path = os.path.join(version_directory,
+                          config.BUILD_ROOT,
+                          implementation["path"])
+  arguments = implementation["arguments"] + instance["arguments"]
+  
+  test_command = sh.Command(exe_path)
+  test_command = test_command.bake(*arguments)
+  
+  input_path = os.path.join(config.DATASET_ROOT, fname)
+  with open(input_path, 'r') as input_file:
+    # time it
+    log_directory = os.path.join(version_directory, "log")
+    # TODO: get rid of this if we don't reuse build directories
+    try:
+      os.mkdir(log_directory)
+    except OSError:
+      # directory already exists -- ignore
+      pass
+    
+    prefix = fname + "_" + str(iteration)
+    start_time = time.time()
+    result = test_command(_in=input_file,
+                 _out=os.path.join(log_directory, prefix + ".out"),
+                 _err=os.path.join(log_directory, prefix + ".err"))
+    end_time = time.time()
+    if result.exit_code != 0:
+      raise ExitCodeException(result.exit_code)
+    
+    time_elapsed = end_time - start_time
+    return time_elapsed
+    
+def runTests(tests):
+  for case_name, case_config in tests.items():
+    print(case_name)
+    
+    result_fname = os.path.join(config.RESULT_ROOT, case_name + ".csv") 
+    with open(result_fname, 'w') as result_file:
+      fieldnames = ["test", "file", "iteration", "time_elapsed"]
+      result_writer = csv.DictWriter(result_file,fieldnames=fieldnames)
+      result_writer.writeheader()
+      iterations = case_config["iterations"]
+      for fname in case_config["files"]:
+        print("\t", fname, ": ", end="")
+        for i in range(iterations):
+          print(i, " ", end="")
+          for test_name, test in case_config["tests"].items():
+            time_elapsed = runTestInstance(test, fname, i)
+            result = { "test": test_name, 
+                       "file": fname, 
+                       "iteration": i,
+                       "time_elapsed": time_elapsed }
+            result_writer.writerow(result)
+        print("")
+    
+    
+if len(sys.argv) == 1:
+  # no arguments
+  tests = config.TESTS
+else:
+  test_cases = sys.argv[1:]
+  tests = {k : config.TESTS[k] for k in test_cases}
     
 os.chdir(config.PROJECT_ROOT)
 
+# Unbuffered output. This is needed for progress indicators to display correctly,
+# since a new line is printed only at the end of each test.
+class flushfile:
+    def __init__(self, f):
+        self.f = f
+    def write(self, x):
+        self.f.write(x)
+        self.f.flush()
+    def flush(self):
+      # no-op: always flushed
+      pass
+        
+sys.stdout = flushfile(sys.stdout)
+
+print("*** Building ***")
 implementations = findImplementations(tests)
-buildImplementations(implementations)
+# TODO: uncomment/make an option
+#buildImplementations(implementations)
+
+print("*** Running tests ***")
+runTests(tests)
--- src/tests/config.py
+++ src/tests/config.py
@@ -10,18 +10,28 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(SCRIPT_ROOT))
 
 #WORKING_DIRECTORY = os.path.join(PROJECT_DIR, "tmp/tests/")
 WORKING_DIRECTORY = "/tmp/flowsolver_benchmark"
-DATASET_ROOT = os.path.join(PROJECT_ROOT, "src", "TESTS", "graphs")
+DATASET_ROOT = os.path.join(PROJECT_ROOT, "src", "tests", "graphs")
+RESULT_ROOT = os.path.join(PROJECT_ROOT, "benchmark")
 BUILD_ROOT = "build"
 SOURCE_ROOT = "src"
 
 # This variable is not used by the suite at all: it is just for convenience
-# within the config for referencing particular FILES
+# within the config for referencing particular files
 FILES = {
-  "synthetic": ["graph_1000m_32j_100t_10p.in", 
-                "graph_100m_16j_100t_10p.in", "graph_100m_8j_100t_10p.in"],
+  # these are so small to be useful for a benchmark: but handy to quickly test
+  # the benchmark script itself
+  "development_only": ["graph_4m_2crs_10j.in", "small_graph.in"],
+  "synthetic_small": ["graph_100m_8j_100t_10p.in", "graph_100m_16j_100t_10p.in"],
+  "synthetic_large": ["graph_1000m_32j_100t_10p.in"],
   "google": ["google_all.in"],
 }
-FILES["all"] = sum(FILES.values(), [])
+
+FILES["synthetic"] = FILES["synthetic_small"] + FILES["synthetic_large"]
+
+all_files = set()
+for files in FILES.values():
+  all_files.update(files)
+FILES["all"] = all_files
 
 IMPLEMENTATIONS = {
   "cs_latest": {
@@ -43,13 +53,13 @@ IMPLEMENTATIONS = {
     "arguments" : ["cycle_cancelling"]
    },
   "cs_wave": {
-   "version": "31a0d47",
+   "version": "cs_wave",
    "target": "find_min_cost",
    "path": "bin/find_min_cost",
    "arguments" : ["cost_scaling"]
   },
   "cs_vertexqueue": {
-   "version": "7bbf977",
+   "version": "cd125be",
    "target": "find_min_cost",
    "path": "bin/find_min_cost",
    "arguments" : ["cost_scaling"]
@@ -57,9 +67,22 @@ IMPLEMENTATIONS = {
 }
 
 TESTS = {
-  "wave_vs_fifo": 
-  {
-    "files": FILES["all"],
+  "development_only": {
+    "files": FILES["development_only"],
+    "iterations": 1,
+    "tests": {
+      "wave": {
+        "implementation": "cs_wave",
+        "arguments": []
+      },
+      "fifo": {
+        "implementation": "cs_vertexqueue",
+        "arguments": []
+      },
+    },
+  },
+  "wave_vs_fifo": {
+    "files": FILES["synthetic"],
     "iterations": 5,
     "tests": {
       "wave": {
