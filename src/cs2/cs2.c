d0b2aa2cb80b579b42898139d4ac8bcf501d1509
Adam Gleave <adgleave@gmail.com>
2015-03-22 14:58:21 +0000

    [cs2] Log time in same format as other solvers

--- src/cs2/cs2.c
+++ src/cs2/cs2.c
@@ -1930,7 +1930,7 @@ int main (int argc, char **argv)
   
   
   printf ("c time:  %15.2f     cost:  %15.0f\n", t, cost);
-  fprintf(stderr, "ALGOTIME: %f\n", t);
+  fprintf(stderr, "ALGOTIME: %.6f\n", t);
   
   printf ("c refines:    %10ld     discharges: %10ld\n",
 	  n_refine, n_discharge);
