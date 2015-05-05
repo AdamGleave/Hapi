59cae0ea9ad9cdbaef990c8fc1ff95b5b03538da
Adam Gleave <adgleave@gmail.com>
2015-05-05 21:11:18 +0100

    [diss] Polish Eval:Optimisation:Relaxation

--- doc/scripts/caelum_fetch_benchmarks.sh
+++ doc/scripts/caelum_fetch_benchmarks.sh
@@ -3,7 +3,7 @@
 OPTIONS="-v --delete -r -lpt"
 EXCLUDES='--include=*.csv --exclude=*'
 OPTIONS="$OPTIONS $EXCLUDES"
-SRC_DIR=/home/srguser/adam/project/benchmark/
+SRC_DIR=/home/srguser/adam/benchmark_results/
 DEST_DIR=/home/adam/project/doc/data/
 
-rsync $OPTIONS srguser@caelum-402.cl.cam.ac.uk:$SRC_DIR $DEST_DIR
+rsync $OPTIONS srguser@caelum-401.cl.cam.ac.uk:$SRC_DIR $DEST_DIR
--- doc/scripts/caelum_fetch_figures.sh
+++ doc/scripts/caelum_fetch_figures.sh
@@ -6,4 +6,6 @@ OPTIONS="$OPTIONS $EXCLUDES"
 SRC_DIR=/home/srguser/adam/project/doc/figures/
 DEST_DIR=/home/adam/project/doc/figures/
 
-rsync $OPTIONS srguser@caelum-401.cl.cam.ac.uk:$SRC_DIR $DEST_DIR
+if [[ `hostname` -ne "ADAM-PETREL" ]]; then
+  rsync $OPTIONS srguser@caelum-401.cl.cam.ac.uk:$SRC_DIR $DEST_DIR
+fi
