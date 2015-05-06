7c5de465e6ac6019067f93974282cd46c4127dba
Adam Gleave <adgleave@gmail.com>
2015-05-06 12:12:54 +0100

    [scripts] Bugfix: be able to cut first snapshot out of imin file

--- src/scripts/cut_snapshot.sh
+++ src/scripts/cut_snapshot.sh
@@ -9,9 +9,14 @@ fi
 SNAPSHOT_N=$1
 FILE=$2
 
-LINES=`grep -n "c EOI" $FILE | head -n $SNAPSHOT_N | tail -n 2 | cut -d: -f1`
-LINES=($LINES)
-START_LINE=`expr ${LINES[0]} + 1`
-END_LINE=${LINES[1]}
+if [[ $SNAPSHOT_N -eq 1 ]]; then
+  START_LINE=1
+  END_LINE=`grep -n "c EOI" $FILE | head -n 1 | cut -d: -f1`
+else
+  LINES=`grep -n "c EOI" $FILE | head -n $SNAPSHOT_N | tail -n 2 | cut -d: -f1`
+  LINES=($LINES)
+  START_LINE=`expr ${LINES[0]} + 1`
+  END_LINE=${LINES[1]}
+fi
 
 sed -n ${START_LINE},${END_LINE}p $FILE
