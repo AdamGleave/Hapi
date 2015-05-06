224b5de17f33427f3076f7ed577e6dadf572ceca
Adam Gleave <adgleave@gmail.com>
2015-05-06 21:48:56 +0100

    [test data] Bugfix: access params relative to script path

--- src/graphs/general/synthetic/generate_inc.sh
+++ src/graphs/general/synthetic/generate_inc.sh
@@ -9,16 +9,16 @@ PARALLEL_ARGS="--eta --jobs -2"
 mkdir $OUTPUT
 cd $OUTPUT
 # Write .param files describing each graph to be generated
-for param_script in ../params/*.sh; do
+for param_script in $SCRIPTPATH/params/*.sh; do
   $param_script
 done
 
 # {.} drops last suffix 
 ls *.param | $PARALLEL $PARALLEL_ARGS "$GENERATOR < {} > {.}" 
 
-mkdir $OUTPUT_approx 
-cd $OUTPUT_approx
-../params_approx.sh
+mkdir ${OUTPUT}_approx 
+cd ${OUTPUT}_approx
+$SCRIPTPATH/params_approx.sh
 
 # {.} drops last suffix 
 ls *.param | $PARALLEL $PARALLEL_ARGS "$GENERATOR < {} > {.}" 
