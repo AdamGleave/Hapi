2f181c8c7d841c62d525a4def4fcafad8cafa810
Adam Gleave <adgleave@gmail.com>
2015-05-09 13:46:50 +0100

    [benchmark] Run on at most N-4 cores

--- src/scripts/approx_generator.sh
+++ src/scripts/approx_generator.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 HELPER=$HOME/adam/project/src/scripts/approx_generator_helper.sh
+PARALLEL_OPTS=-j -4
 
 SEED_FILE=$1
 OUTPUT_DIR=$2
@@ -13,4 +14,4 @@ let DELTA=$END_SEED-$START_SEED+1
 
 # -n: adds line numbers
 cat -n $SEED_FILE | head -n $END_SEED | tail -n $DELTA \
-                  | parallel $HELPER $OUTPUT_DIR {} 
+                  | parallel $PARALLEL_OPTS $HELPER $OUTPUT_DIR {} 
