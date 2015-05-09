3033a08d15955c9e021507920a7fec9a5609f5ad
Adam Gleave <adgleave@gmail.com>
2015-05-09 12:10:04 +0100

    [simulator] Bugfix in approximate scripts

--- src/scripts/approx_generator.sh
+++ src/scripts/approx_generator.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-SIMULATOR=$HOME/adam/firmament/build/sim/trace-extract/google_trace_simulator
-PERCENTAGE=25
+HELPER=$HOME/adam/project/src/scripts/approx_generator_helper.sh
 
 SEED_FILE=$1
 OUTPUT_DIR=$2
@@ -10,18 +9,8 @@ END_SEED=$4
 
 mkdir -p $OUTPUT_DIR
 
-# -n: adds line numbers
 let DELTA=$END_SEED-$START_SEED+1
 
-cat -n $SEED_FILE | head -n $END_SEED | tail -n $DELTA | parallel  
-for (( s=$START_SEED; s<=$END_SEED; s++ ))
-do
-  $SIMULATOR -logtostderr -batch_step 10 -flow_scheduling_cost_model 8 \
-             -machine_tmpl_file /home/srguser/adam/firmament/tests/testdata/michael.pbin \
-             -solver custom -solver_timeout 60 \
-             -flow_scheduling_binary ~/adam/project/build/cs2/cs2 \
-             -incremental_flow=False -trace_path /mnt/data/adam/traces/small \
-             -num_files_to_process 1 -max_scheduling_rounds 1 \
-             -percentage $PERCENTAGE -graph_output_file ${OUTPUT_DIR}/${s}.min
-             -simulated_quincy_random_seed $SEED
-done
+# -n: adds line numbers
+cat -n $SEED_FILE | head -n $END_SEED | tail -n $DELTA \
+                  | parallel $HELPER $OUTPUT_DIR {} 
--- src/scripts/approx_generator_helper.sh
+++ src/scripts/approx_generator_helper.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
+SIMULATOR=$HOME/adam/firmament/build/sim/trace-extract/google_trace_simulator
 PERCENTAGE=25
 
-INDEX=$1
-SEED=$2
+OUTPUT_DIR=$1
+ARGS=$2
+read -ra PARSED_ARGS <<< "$ARGS"
+INDEX=${PARSED_ARGS[0]}
+SEED=${PARSED_ARGS[1]]}
 
 $SIMULATOR -logtostderr -batch_step 10 -flow_scheduling_cost_model 8 \
  -machine_tmpl_file /home/srguser/adam/firmament/tests/testdata/michael.pbin \
@@ -9,5 +15,5 @@ $SIMULATOR -logtostderr -batch_step 10 -flow_scheduling_cost_model 8 \
  -flow_scheduling_binary ~/adam/project/build/cs2/cs2 \
  -incremental_flow=False -trace_path /mnt/data/adam/traces/small \
  -num_files_to_process 1 -max_scheduling_rounds 1 \
- -percentage $PERCENTAGE -graph_output_file ${OUTPUT_DIR}/${INDEX}.min
- -simulated_quincy_random_seed $SEED
+ -percentage $PERCENTAGE -graph_output_file ${OUTPUT_DIR}/${INDEX}.min \
+ -simulated_quincy_random_seed $SEED \
